d6c9484c116939a4ee4fb485acc5f7ccfa9adac5
aaaaaaaaaaaaaaassda <107585267+aaaaaaaaaaaaaaassda@users.noreply.github.com>
2022-06-16 01:08:16 +0300

    Add files via upload

---
+++ asdgfasg/app/commands/core/help.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'help',
+    aliases: ['h',"yardım","y"],
+    showHelp: false,
+    utilisation: '{prefix}help',
+
+    execute(client, message, args) {
+        const embed = new MessageEmbed();
+
+        embed.setColor('RED');
+        embed.setAuthor(client.user.username, client.user.displayAvatarURL({ size: 1024, dynamic: true }));
+
+        const commands = client.commands.filter(x => x.showHelp !== false);
+
+        embed.setDescription('Ψ O L Y M P O S Ψ');
+        embed.addField(`Kullanıla Bilir - ${commands.size} Komut Var`, commands.map(x => `\`${x.name}${x.aliases[0] ? ` (${x.aliases.map(y => y).join(', ')})\`` : '\`'}`).join(' | '));
+
+        embed.setTimestamp();
+        embed.setFooter('Ψ O L Y M P O S  Müzik Botu', message.author.avatarURL({ dynamic: true }));
+
+        message.channel.send({ embeds: [embed] });
+    },
+};
